c1d631412e170ef041ad3cbb226b76f0d8f76f91
Marília S <42978269+Maliarte@users.noreply.github.com>
2023-12-07 15:52:58 -0300

    Update Cliente.java Validações

--- av2/Cliente.java
+++ av2/Cliente.java
@@ -1,15 +1,18 @@
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Cliente {
     private int idCliente;
     private String nome;
     private String endereco;
     private String postalCode;
     private String pais;
-    private int cpf; 
+    private String cpf; // Alterado para String para armazenar corretamente o CPF
     private String passaporte;
     private String email;
     private String dataNascimento;
 
-    // Getters e setters
+    // Getters e setters com validações
     public int getIdCliente() {
         return idCliente;
     }
@@ -50,12 +53,17 @@ public class Cliente {
         this.pais = pais;
     }
 
-    public int getCpf() {
+    public String getCpf() {
         return cpf;
     }
 
-    public void setCpf(int cpf) {
-        this.cpf = cpf;
+    // Validar e formatar o CPF
+    public void setCpf(String cpf) {
+        if (validarCPF(cpf)) {
+            this.cpf = formatarCPF(cpf);
+        } else {
+            System.out.println("CPF inválido. Não será definido.");
+        }
     }
 
     public String getPassaporte() {
@@ -81,4 +89,57 @@ public class Cliente {
     public void setDataNascimento(String dataNascimento) {
         this.dataNascimento = dataNascimento;
     }
+
+    // Função para validar o CPF
+    private boolean validarCPF(String cpf) {
+    // Remover pontuações e espaços do CPF
+    cpf = cpf.replaceAll("[^0-9]", "");
+
+    // Verificar se o CPF possui 11 dígitos
+    if (cpf.length() != 11) {
+        return false;
+    }
+
+    // Calcular e verificar os dígitos verificadores
+    int[] numeros = new int[11];
+    for (int i = 0; i < 11; i++) {
+        numeros[i] = Character.getNumericValue(cpf.charAt(i));
+    }
+
+    // Verificar primeiro dígito verificador
+    int soma = 0;
+    for (int i = 0; i < 9; i++) {
+        soma += numeros[i] * (10 - i);
+    }
+    int resto = soma % 11;
+    int digitoVerificador1 = (resto < 2) ? 0 : (11 - resto);
+    if (numeros[9] != digitoVerificador1) {
+        return false;
+    }
+
+    // Verificar segundo dígito verificador
+    soma = 0;
+    for (int i = 0; i < 10; i++) {
+        soma += numeros[i] * (11 - i);
+    }
+    resto = soma % 11;
+    int digitoVerificador2 = (resto < 2) ? 0 : (11 - resto);
+    if (numeros[10] != digitoVerificador2) {
+        return false;
+    }
+
+    return true;
+   }
+
+    private String formatarCPF(String cpf) {
+    // Verificar se o CPF é válido antes de formatar
+    if (validarCPF(cpf)) {
+        // Adicionar pontuações ao CPF
+        return cpf.substring(0, 3) + "." + cpf.substring(3, 6) + "." + cpf.substring(6, 9) + "-" + cpf.substring(9);
+    } else {
+        System.out.println("CPF inválido. Não será formatado.");
+        return cpf;
+    }
+}
+
 }
